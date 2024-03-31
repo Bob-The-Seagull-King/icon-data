a2a2fd7afacc8789ac86ad2cab1e22de55371d4c
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-04-01 10:00:22 +1300

    documentation / comments added

--- util/DataResponder.ts
+++ util/DataResponder.ts
@@ -1,52 +1,60 @@
 
+// Data File Imports -----------------------------------
 import abilitiesdata from '../data/player/abilities.json'
 import addonsdata from '../data/player/addons.json'
+// -----------------------------------------------------
 
+/**
+ * Basic request interface
+ */
 interface IDataRequest {
-    type: string
+    type: string // The data file to search within
 }
 
+/**
+ * Used for single-id collection and return-all-values-by-key collection
+ */
 interface IDataRequestID extends IDataRequest {
-    id: string
+    id: string // The ID value being selected for
 }
 
+/**
+ * Used for complex searches
+ */
 interface IDataRequestComplexSearch extends IDataRequest {
-    request: IDataRequestSearchParam
+    request: IDataRequestSearchParam // The search reques to filter items by
 }
 
+/**
+ * Used for complex searches to combine and contain search terms
+ */
 interface IDataRequestSearchParam {
-    operator: string, // AND, OR
+    operator: string, // Can be 'and' (true if all terms and subparams return true) or 'or' (true if one term or subparam returns true)
     terms: IDataRequestSearchTerm[],
     subparams: IDataRequestSearchParam[]
 }
 
+/**
+ * Used to validate if a given data item fits search criteria
+ */
 interface IDataRequestSearchTerm {
-    item: string,
-    value: any,
+    item: string, // The string name of the key being checked
+    value: any, // The desired value of the key
     equals: boolean, // true -> check if item == value, false -> check if item != value
     istag?: boolean, // true -> checks if a tag with tag_name=value has tagvalue
     tagvalue?: any, // if left blank, just checks if tag_name exists
 }
 
+/**
+ * Static-only class that returns desired JSON data
+ */
 class DataResponder {
 
-    public static GetSingleEntry(request: IDataRequestID) {
-        const dataSet = DataResponder.GetDataType(request.type)
-
-        let i = 0;
-        for (i = 0; i < dataSet.length; i++) {
-            if (dataSet[i].id == request.id) {
-                return dataSet[i]
-            }
-        }
-        return []
-    }
-
-    public static GetFullDataEntry(request: IDataRequest) {
-        const dataSet = DataResponder.GetDataType(request.type)
-        return dataSet;
-    }
-
+    /**
+     * Grabs the correct json file to search.
+     * @param type The type of data to be searched
+     * @returns JSON array of data to search
+     */
     private static GetDataType(type: string) {
         switch(type) {
             case "abilities": {
@@ -64,6 +72,59 @@ class DataResponder {
         }
     }
 
+    /**
+     * Grabs a single entry based on ID value
+     * @param request JSON structure that provides the file to search and the ID to search by
+     * @returns JSON object, either empty or containing the found entry
+     */
+    public static GetSingleEntry(request: IDataRequestID) {
+        const dataSet = DataResponder.GetDataType(request.type)
+
+        let i = 0;
+        for (i = 0; i < dataSet.length; i++) {
+            if (dataSet[i].id == request.id) {
+                return dataSet[i]
+            }
+        }
+        return {}
+    }
+
+    /**
+     * Gets an entire data file
+     * @param request Contains the name of the file to grab
+     * @returns JSON array of all data in the specified file
+     */
+    public static GetFullDataEntry(request: IDataRequest) {
+        const dataSet = DataResponder.GetDataType(request.type)
+        return dataSet;
+    }
+
+    /**
+     * Grabs all values of a given key in a data file
+     * @param request The file to search and the name of the key to get values from
+     * @returns Array of all values associated with the requested key in the requested file
+     */
+    public static GetAllOfKeyInData(request: IDataRequestID) {
+        const dataSet = DataResponder.GetDataType(request.type)
+        const valueSet = []
+
+        let i = 0;
+        for (i = 0; i < dataSet.length; i++) {
+            const data = dataSet[i];
+            const dynamicKey = request.id as keyof (typeof data);
+            valueSet.push(data[dynamicKey])
+        }
+
+        return valueSet;
+    }
+
+    /**
+     * Performs a complex search with a variety of filtering terms
+     * by iterating through each entry in a data file and validating
+     * if they meet the criteria
+     * @param search Determines the file to search and criteria to search by
+     * @returns JSON array of entries in a data file that match the criteria
+     */
     public static ComplexSearch(search: IDataRequestComplexSearch) {
         const dataSet = DataResponder.GetDataType(search.type)
         const dataSelect = []
@@ -74,10 +135,16 @@ class DataResponder {
                 dataSelect.push(dataSet[i]);
             }
         }
-
         return dataSelect;
     }
 
+    /**
+     * Validates if a data object should be included in the search based on a
+     * parameter that combines multiple search terms with AND or OR operators.
+     * @param term the list of search terms and sub-params
+     * @param data the data object to check
+     * @returns boolean if the data match all search criteria
+     */
     public static ValidateComplexSearch(term: IDataRequestSearchParam, data: any) {
         let isvalid = false;
         let i = 0;
@@ -96,7 +163,6 @@ class DataResponder {
                 }
             }
         }
-        
 
         for (i = 0; i < term.subparams.length; i++) {
             const isSearch = DataResponder.ValidateComplexSearch(term.subparams[i], data)
@@ -116,6 +182,12 @@ class DataResponder {
         return isvalid;
     }
 
+    /**
+     * Checks if the values of a data object match a single desired search term
+     * @param term The desired key to check, value to want, and other requirements
+     * @param data The data object being validated
+     * @returns boolean if the data object matches the search term
+     */
     public static ValidateBySearch(term: IDataRequestSearchTerm, data: any) {
         if (!term.istag) {
             const dynamicKey = term.item as keyof (typeof data);
@@ -148,9 +220,6 @@ class DataResponder {
             }
         }
     }
-    // Turn search term into check
-
-    // Run through data, for each entry, do all the above checks
 }
 
 export {DataResponder}
