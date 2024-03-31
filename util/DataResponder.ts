d1d859b676825f43fef3737155d43ae5fda95226
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-03-31 22:52:59 +1300

    search request

--- util/DataResponder.ts
+++ util/DataResponder.ts
@@ -10,6 +10,24 @@ interface IDataRequestID extends IDataRequest {
     id: string
 }
 
+interface IDataRequestComplexSearch extends IDataRequest {
+    request: IDataRequestSearchParam
+}
+
+interface IDataRequestSearchParam {
+    operator: string, // AND, OR
+    terms: IDataRequestSearchTerm[],
+    subparams: IDataRequestSearchParam[]
+}
+
+interface IDataRequestSearchTerm {
+    item: string,
+    value: any,
+    equals: boolean, // true -> check if item == value, false -> check if item != value
+    istag?: boolean, // true -> checks if a tag with tag_name=value has tagvalue
+    tagvalue?: any, // if left blank, just checks if tag_name exists
+}
+
 class DataResponder {
 
     public static GetSingleEntry(request: IDataRequestID) {
@@ -45,6 +63,96 @@ class DataResponder {
             }
         }
     }
+
+    public static ComplexSearch(search: IDataRequestComplexSearch) {
+        const dataSet = DataResponder.GetDataType(search.type)
+        const dataSelect = []
+
+        let i = 0;
+        for (i = 0; i < dataSet.length; i++) {
+            if (DataResponder.ValidateComplexSearch(search.request, dataSet[i])) {
+                dataSelect.push(dataSet[i]);
+            }
+        }
+
+        return dataSelect;
+    }
+
+    public static ValidateComplexSearch(term: IDataRequestSearchParam, data: any) {
+        let isvalid = false;
+        let i = 0;
+
+        console.log(data)
+        
+        for (i = 0; i < term.terms.length; i++) {
+            const isSearch = DataResponder.ValidateBySearch(term.terms[i], data)
+            if (term.operator == "and") {
+                if (isSearch == false) {
+                    return false;
+                } else {
+                    isvalid = true;
+                }
+            } else {
+                if (isSearch) {
+                    isvalid = true;
+                }
+            }
+        }
+        
+
+        for (i = 0; i < term.subparams.length; i++) {
+            const isSearch = DataResponder.ValidateComplexSearch(term.subparams[i], data)
+            if (term.operator == "and") {
+                if (isSearch == false) {
+                    return false;
+                } else {
+                    isvalid = true;
+                }
+            } else {
+                if (isSearch) {
+                    isvalid = true;
+                }
+            }
+        }
+
+        return isvalid;
+    }
+
+    public static ValidateBySearch(term: IDataRequestSearchTerm, data: any) {
+        if (!term.istag) {
+            const dynamicKey = term.item as keyof (typeof data);
+            if (term.equals) {
+                return (data[dynamicKey] == term.value)
+            } else {
+                return (data[dynamicKey] != term.value)
+            }
+        } else {
+            const dynamicKey = term.item as keyof (typeof data);
+            if (term.tagvalue == "") {
+                let i = 0;
+                for (i = 0; i < data[dynamicKey].length; i++) {
+                    if (data[dynamicKey][i].tag_name == term.value) {
+                        return (term.equals == true)
+                    }
+                }
+                return false;
+            } else {
+                
+                let i = 0;
+                for (i = 0; i < data[dynamicKey].length; i++) {
+                    if (data[dynamicKey][i].tag_name == term.value) {
+                        if (data[dynamicKey][i].val == term.tagvalue) {
+                            return (term.equals == true)
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    // Turn search term into check
+
+    // Run through data, for each entry, do all the above checks
 }
 
 export {DataResponder}
