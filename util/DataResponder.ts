915c28fffa8b1dd842e48707e723a4d80d050607
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-04-05 21:53:03 +1300

    hotfix

--- util/DataResponder.ts
+++ util/DataResponder.ts
@@ -266,7 +266,7 @@ class DataResponder {
                         }
                     }
                 }
-                return false;
+                return (term.equals == false);
             }
         }
     }
